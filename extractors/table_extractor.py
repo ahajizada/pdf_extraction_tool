21d2aac14b731b2487c64d8001859525665b42fb
Abulfaz Hajizada <66151753+ahajizada@users.noreply.github.com>
2025-01-13 18:19:25 +0400

    Update table_extractor.py

--- extractors/table_extractor.py
+++ extractors/table_extractor.py
@@ -3,9 +3,8 @@ import pdfplumber
 def extract_tables(file_path):
     tables = []
     with pdfplumber.open(file_path) as pdf:
-        for page in pdf.pages:
+        for page_num, page in enumerate(pdf.pages, start=1):
             table = page.extract_table()
             if table:
-                tables.append(table)
+                tables.append({"page": page_num, "table": table})
     return tables
-
