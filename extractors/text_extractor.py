e3ee53acf2883416dfbe40690c2897b211fb9199
Abulfaz Hajizada <66151753+ahajizada@users.noreply.github.com>
2025-01-13 18:16:33 +0400

    Update text_extractor.py

--- extractors/text_extractor.py
+++ extractors/text_extractor.py
@@ -1,1 +1,14 @@
+import fitz  
+
+def extract_text(file_path, keyword=None):
+    text_data = []
+    doc = fitz.open(file_path)
+    for page in doc:
+        text = page.get_text()
+        if keyword:
+            if keyword in text:
+                text_data.append(text)
+        else:
+            text_data.append(text)
+    return text_data
 
