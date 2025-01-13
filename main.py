54be61fbd79e2cb235cf09bed4b504f7098a19db
Abulfaz Hajizada <66151753+ahajizada@users.noreply.github.com>
2025-01-13 18:19:49 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import argparse
-from extractors.text_extractor import extract_text
-from extractors.table_extractor import extract_tables
 import pandas as pd
 import json
+from extractors.text_extractor import extract_text
+from extractors.table_extractor import extract_tables
 
 def main():
     parser = argparse.ArgumentParser(description="PDF Data Extraction Tool")
@@ -16,8 +16,9 @@ def main():
 
     if args.output == "csv":
         if tables:
-            df = pd.DataFrame(tables[0])
-            df.to_csv("output/extracted_data.csv", index=False)
+            for table in tables:
+                df = pd.DataFrame(table["table"])
+                df.to_csv(f"output/extracted_table_page_{table['page']}.csv", index=False)
         else:
             with open("output/extracted_data.csv", "w") as f:
                 f.writelines(text_data)
@@ -26,8 +27,7 @@ def main():
         with open("output/extracted_data.json", "w") as f:
             json.dump(data, f, indent=4)
 
-    print(f"Data extracted and saved in output/extracted_data.{args.output}")
+    print(f"Data extracted and saved in the 'output' directory.")
 
 if __name__ == "__main__":
     main()
-
